38358320dcb022ab57d9abb9af2fadf1ceb08d7b
Hero <hero4barca@gmail.com>
2021-12-17 04:06:37 +0100

    initial commit with template integration and site app

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-x)tex3$f&+*c3qd9y0j^)y!fby**k9*2&^wq46^4ycw4+n_)js'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ site_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+class IndexPage (Page):
+    video_text = RichTextField(blank= True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('video_text', classname="full")
+    ]
