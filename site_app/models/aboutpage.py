14e198007ae2cd517f09f29d054ba75cb3f58faf
Hero <hero4barca@gmail.com>
2021-12-21 03:54:02 +0100

    finish goals section on about page template

--- site_app/models/aboutpage.py
+++ site_app/models/aboutpage.py
@@ -111,9 +111,7 @@ class AboutPage(Page):
     ati_tile5_text = RichTextField( blank=True)
     ati_tile6_title = models.CharField(max_length= 50, default="Tile 6")
     ati_tile6_text = RichTextField( blank=True)
-
-
-    
+        
 
     # //Call to action
     call_to_action = models.BooleanField( default=True)
@@ -124,25 +122,73 @@ class AboutPage(Page):
     CTA_button2_text = models.CharField (max_length=25, blank=True)
     CTA_button2_link = models.CharField (max_length=250, default="#")
 
-    
+    # //Goals section
+    goals_title_text = RichTextField( blank=True)
+
+    goals_card1_title = models.CharField( max_length=25, default="Our Mission" )
+    goals_card1_text = RichTextField( blank=True)
+    goals_card1_img = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    goals_card2_title = models.CharField( max_length=25, default="Our Plan" )
+    goals_card2_text = RichTextField( blank=True)
+    goals_card2_img = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    goals_card3_title = models.CharField( max_length=25, default="Our Vission" )
+    goals_card3_text = RichTextField( blank=True)
+    goals_card3_img = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    goals_card4_title = models.CharField( max_length=25, default="Our Plan" )
+    goals_card4_text = RichTextField( blank=True)
+    goals_card4_img = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
 
 
 
 
     content_panels = Page.content_panels + [
+
+
+        #// slider section
+
+        # // slide 1
         FieldPanel ('first_slide_h1'),
         FieldPanel ('first_slide_h2', classname="full"),
         FieldPanel ('first_slide_button_text'),
         FieldPanel ('first_slide_button_link'),
         ImageChooserPanel('first_slide_img'),
 
+        # // slide 2
         FieldPanel ('second_slide_h1'),
         FieldPanel ('second_slide_h2', classname="full"),
         FieldPanel ('second_slide_button_text'),
         FieldPanel ('second_slide_button_link'),
         ImageChooserPanel('second_slide_img'),
 
+        # // slide 3
         FieldPanel('third_slide'),
         FieldPanel ('third_slide_h1'),
         FieldPanel ('third_slide_h2', classname="full"),
@@ -150,6 +196,7 @@ class AboutPage(Page):
         FieldPanel ('third_slide_button_link'),
         ImageChooserPanel('third_slide_img'),
 
+        #  // slide 4
         FieldPanel('fourth_slide'),
         FieldPanel ('fourth_slide_h1'),
         FieldPanel ('fourth_slide_h2', classname="full"),
@@ -157,6 +204,7 @@ class AboutPage(Page):
         FieldPanel ('fourth_slide_button_link'),
         ImageChooserPanel('fourth_slide_img'),
 
+        # slide 5
         FieldPanel('fifth_slide'),
         FieldPanel ('fifth_slide_h1'),
         FieldPanel ('fifth_slide_h2', classname="full"),
@@ -164,29 +212,35 @@ class AboutPage(Page):
         FieldPanel ('fifth_slide_button_link'),
         ImageChooserPanel('fifth_slide_img'),
 
+        # // ============== about section - video  ===============
         FieldPanel('video_section_title', classname="full"),
         FieldPanel('video_section_text', classname="full"),
         FieldPanel('video_url', classname="full"),
         ImageChooserPanel('video_thumbnail_img'),
 
+        # // ============== about section - iconed tiles ===================
+        
+        # // first row
         FieldPanel('excellence_title'),
         FieldPanel('excellence_text',  classname="full"),
         FieldPanel('compassion_title'),
         FieldPanel('compassion_text',  classname="full"),
 
+        # // second row
         FieldPanel('ati_second_row'),
         FieldPanel('ati_tile3_title'),
         FieldPanel('ati_tile3_text',  classname="full"),
         FieldPanel('ati_tile4_title'),
         FieldPanel('ati_tile4_text',  classname="full"),
 
+        # // third row
         FieldPanel('ati_third_row'),
         FieldPanel('ati_tile5_title'),
         FieldPanel('ati_tile5_text',  classname="full"),
         FieldPanel('ati_tile6_title'),
         FieldPanel('ati_tile6_text',  classname="full"),
 
-
+        # // =============== call to action section ======================
         FieldPanel('call_to_action'),
         FieldPanel ('CTA_title', classname="full"),
         FieldPanel ('CTA_text', classname="full"),
@@ -194,7 +248,31 @@ class AboutPage(Page):
         FieldPanel ('CTA_button1_link',  ),
         FieldPanel ('CTA_button2_text', classname="full" ),
         FieldPanel ('CTA_button2_link',  ),            
-            
+
+        # // ================== Goals section====================
+        
+        FieldPanel('goals_title_text', classname="full"),
+        
+        # card 1
+        FieldPanel('goals_card1_title'),
+        FieldPanel('goals_card1_text', classname="full"),
+        ImageChooserPanel('goals_card1_img'),
+
+        # card 2
+        FieldPanel('goals_card2_title'),
+        FieldPanel('goals_card2_text', classname="full"),
+        ImageChooserPanel('goals_card2_img'),
+
+        # card 3
+        FieldPanel('goals_card3_title'),
+        FieldPanel('goals_card3_text', classname="full"),
+        ImageChooserPanel('goals_card3_img'),
+
+        # card 4
+        FieldPanel('goals_card4_title'),
+        FieldPanel('goals_card4_text', classname="full"),
+        ImageChooserPanel('goals_card4_img'),
+        
             ]
 
 AboutPage._meta.get_field('ati_second_row').help_text = "Show/hide the second row of the iconed tiles"
