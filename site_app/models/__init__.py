383367e168d805f89e107ae73b65ddcec48aca5a
Hero <hero4barca@gmail.com>
2021-12-30 03:39:24 +0100

    course detail page -initial

--- site_app/models/__init__.py
+++ site_app/models/__init__.py
@@ -2,4 +2,4 @@ from .index_page import IndexPage
 from .about_page import AboutPage
 from .contact_page import ContactPage
 from .main_course_page import MainCoursePage
-
+from .course_detail_page import CourseDetailPage
---
+++ site_app/models/course_detail_page.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.db.models.fields import URLField
+from wagtail.admin.edit_handlers import StreamFieldPanel, FieldPanel
+from wagtail.core import blocks
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+class CourseDetailPage(Page):
+    
+    class HeaderTopBlock(blocks.StructBlock):
+
+        header_image = ImageChooserBlock(required=True)
+        h1_text = blocks.CharBlock(required=False)
+        h2_text = blocks.CharBlock(required=False)
+
+    top_header = StreamField([
+        ('header', HeaderTopBlock())
+            ],
+            null=False,
+            block_counts={
+                            'header': { 'max_num': 1, 'min_num':0 }, # header image is optional
+                                    })
+
+
+    course_title =  models.CharField(default="Course Title Here",
+                                         max_length=250 )
+    
+    
+    
+    COURSE_TYPE_CHOICES = [
+                ('taught course', 'taught course'),
+                ('masterclass', 'masterclass'),
+            ]
+        
+    course_type = models.CharField(choices=COURSE_TYPE_CHOICES, max_length=20)
+
+    course_details_section = RichTextField(default="Provide detailed course description here")
+    course_price = models.DecimalField(decimal_places=2, default=0.0, max_digits=9, max_length=10)
+    course_payment_link = URLField(blank=True)
+
+    overview_and_benefits = RichTextField(blank=True)
+    detailed_curriculum = RichTextField(blank=True)
+
+    about_facilitator = StreamField([
+        ('heading', blocks.CharBlock(form_classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+                ],
+              null=True, blank=True 
+                )
+
+    content_panels = Page.content_panels + [
+
+            StreamFieldPanel('top_header'),
+            FieldPanel('course_title'),
+            FieldPanel('course_details_section'),
+            FieldPanel('course_price'),
+            FieldPanel('course_payment_link'),
+            FieldPanel('course_type'),
+            FieldPanel('overview_and_benefits'),   
+            FieldPanel('detailed_curriculum'),
+            StreamFieldPanel('about_facilitator'),
+            ]
