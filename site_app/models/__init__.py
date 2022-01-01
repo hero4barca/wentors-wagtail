3f4b505e9be7ebcb359c79625938d95ef62ef536
Hero <hero4barca@gmail.com>
2022-01-01 02:33:36 +0100

    added gitignore

--- site_app/models/__init__.py
+++ site_app/models/__init__.py
@@ -3,3 +3,4 @@ from .about_page import AboutPage
 from .contact_page import ContactPage
 from .main_course_page import MainCoursePage
 from .course_detail_page import CourseDetailPage
+from .blog_main_page import BlogMainPage
