42c55e9ad083b2ace5037f3b9d8e45b788dc5e16
Hero <hero4barca@gmail.com>
2022-01-03 09:39:48 +0100

    update db setting for dev and prod settings file

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -94,13 +94,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# moved to dev and prod
 
 
 # Password validation
--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,6 +1,17 @@
 from .base import *
 
+# turn debug to false in production
 DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# set to use postgre backend in production
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mysite_db',
+    }
+}
 
 try:
     from .local import *
