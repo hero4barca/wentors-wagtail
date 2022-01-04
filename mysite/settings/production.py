273214e0d73baffe66700d3ab92a8c2d21621b92
Hero <hero4barca@gmail.com>
2022-01-04 20:16:27 +0100

    added db config for production settings

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 
 # turn debug to false in production
 DEBUG = False
@@ -16,12 +17,15 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # set to use postgre backend in production
+
+DATABASES=[]
+DATABASES['default'] = dj_database_url.parse( os.environ.get("DATABASE_URL"),
+                             conn_max_age=600)
 # DATABASES = {
 #      'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'mysite_db',
-#     }
-# }
+
 
 try:
     from .local import *
