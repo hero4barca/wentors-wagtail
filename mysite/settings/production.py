3d46acb4c152ddf003b8c1a57ccae2d7f4d09be4
Hero <hero4barca@gmail.com>
2022-01-04 23:53:47 +0100

    remove django heroku locals

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -20,14 +20,17 @@ ALLOWED_HOSTS = ['*']
 
 
 
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mysite_db',
-                  }
-              }
+# DATABASES = {
+#      'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mysite_db',
+#                   }
+#   
+#             }
 
-DATABASES['default'] =  dj_database_url.config()
+
+DATABASES['default'] = dj_database_url.parse( os.environ.get("DATABASE_URL"),
+                                                 conn_max_age=600)
 
 try:
     from .local import *
