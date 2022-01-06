9ebbd4db40b9b03666c6cfe4bc116d9ff5867064
Hero <hero4barca@gmail.com>
2022-01-06 20:35:34 +0100

    media files done -> initial

--- mysite/settings/aws_file_storage.py
+++ mysite/settings/aws_file_storage.py
@@ -1,5 +1,7 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
+
+# subclassing s3boto3Storage
 class MediaStorage(S3Boto3Storage):
     location = 'media'
-    file_overwrite = False
+    file_overwrite = False # don't multiple file with same name 
