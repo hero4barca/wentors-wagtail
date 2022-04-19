b2cb1da74cb01f950d6b3d4062b95b38c61dbb58
Hero <hero4barca@gmail.com>
2022-04-19 18:50:08 +0100

    updates to the jop_page UI, updates to job_page model, migrations

--- site_app/models/jobs_page.py
+++ site_app/models/jobs_page.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from wagtail.admin.edit_handlers import StreamFieldPanel, FieldPanel
 from wagtail.core import blocks
@@ -11,4 +12,46 @@ from .custom_blocks import ButtonBlock
 
 class JobsPage(Page):
 
-    pass
+    jobs_partners_descriptrion = RichTextField(default="Jobs partners description here",
+                                            help_text="jobs partners description here")
+
+
+    class JobsPartnerBlock (blocks.StructBlock):
+        
+        job_partner_name = blocks.CharBlock( default="Job Partner Name")
+        job_partner_logo =  ImageChooserBlock(required=True)
+        job_partner_description = blocks.RichTextBlock(default="Provide detailed job partner description here",
+                                            help_text="provide detailed job partner descriprtion description here")
+        job_partner_link = blocks.CharBlock(required=True, default='https://www.wentors.com')
+
+    
+    job_partners = StreamField(
+
+                    [  ( 'jobs_partners', JobsPartnerBlock() ) ], 
+                    null=True, blank=True,
+                    block_counts={ 'jobs_partners': 
+                                        {'min_num': 1},
+                                 },
+                    help_text = "job partner details"
+                    )
+   
+    # about_facilitator = StreamField([
+    #     ('facilitator', AboutFacilitatorBlock() )      
+    #             ],
+    #           null=True,
+    #           blank=True,
+    #           block_counts={
+    #                     'facilitator': { 'max_num': 1}, 
+    #                         },
+    #             help_text="more details about facilitator" 
+    #             )
+
+
+
+    content_panels = Page.content_panels + [
+        
+        FieldPanel('jobs_partners_descriptrion'),
+        StreamFieldPanel('job_partners')
+
+    ]
+
