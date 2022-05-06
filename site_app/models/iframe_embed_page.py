ac95d7dc1fd30b2e2c9df04d7d95bc4654eafce0
Hero <hero4barca@gmail.com>
2022-05-06 15:48:04 +0100

    updates to job page UI, embed page model for  embeding iframes crated

--- site_app/models/iframe_embed_page.py
+++ site_app/models/iframe_embed_page.py
@@ -5,5 +5,6 @@ from wagtail.core.models import Page
 
 class IframeEmbedPage(Page):
 
-    partner_name = models.CharField(max_length=1000, default="partner name")
+    partner_name = models.CharField(max_length=50, default="partner name")
+    iframe_link = models.URLField(max_length=200, default="https://www.wentors.com" )
     
