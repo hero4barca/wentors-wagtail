f67513b858b7549053214246233eb69d591334ed
Hero <hero4barca@gmail.com>
2022-05-10 15:32:41 +0100

    complete jobs page

--- site_app/models/iframe_embed_page.py
+++ site_app/models/iframe_embed_page.py
@@ -1,5 +1,6 @@
 from django.db import models
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 
@@ -7,4 +8,12 @@ class IframeEmbedPage(Page):
 
     partner_name = models.CharField(max_length=50, default="partner name")
     iframe_link = models.URLField(max_length=200, default="https://www.wentors.com" )
+
+
+    content_panels = Page.content_panels + [
+        
+        FieldPanel('partner_name'),
+        FieldPanel('iframe_link')
+
+    ]
     
